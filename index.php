a668d1ec9ede64cfab2542002c3d8b174ff878fc
MasamiGit <info@masami.main.jp>
2018-06-29 23:12:50 +0900

    ブランチの操作

--- index.php
+++ index.php
@@ -43,7 +43,22 @@
 	echo PHP_VERSION;
 
 	?>
-
+<h3>ブランチの作成</h3>
+<p>git branch ブランチの名前</p>
+<h3>ブランチ切り替え</h3>
+<p>git checkout ブランチの名前</p>
+<h3>ブランチの作成と切り替えを同時に行う</h3>
+<p>git checkout -b ブランチの名前</p>
+<h3>ブランチ削除</h3>
+<p>git branch -d ブランチの名前</p>
+<h3>ブランク名の変更</h3>
+<p>git branch -m ブランチの名前</p>
+<h3>ローカルブランチの一覧</h3>
+<p>git branch</p>
+<h3>リモートとローカルのブランチ一覧</h3>
+<p>git branch -a</p>
+<h3>リモートブランチの一覧</h3>
+<p>git branch -r</p>
 
 </body> 
 </html>
